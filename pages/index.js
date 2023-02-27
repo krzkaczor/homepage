3e3291599acd3684fbfa9e50b9cba3455ae47432
Krzysztof Kaczor <krzkaczor@gmail.com>
2023-02-27 11:18:38 +0100

    init

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>kaczor.io</title>
+      </Head>
+      <main>
+        <h1>kaczor.io</h1>
+        <p>
+          <a href="https://twitter.com/krzkaczor">Twitter</a> | <a href="https://github.com/krzkaczor">Github</a>
+        </p>
+      </main>
+    </>
+  );
+}
